3171175e54a89b9f75dae42633c22563eaad19a0
zhouzheng <programmarzheng@163.com>
2026-08-01 21:34:05 +0800

    refactor(config): add DashScopeApiProperties, unify API key injection in DashScopeLlmProvider

--- SuperBizAgent-release-2026-05-17/src/main/java/org/example/Main.java
+++ SuperBizAgent-release-2026-05-17/src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import org.example.config.DashScopeApiProperties;
 import org.example.config.ModelProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ModelProperties.class})
+@EnableConfigurationProperties({ModelProperties.class, DashScopeApiProperties.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
